e986f4032653c5fdee5c2f225d0d92f07e0e2825
jjvolesky <60017217+jjvolesky@users.noreply.github.com>
2025-03-30 12:56:14 -0600

    new download list

--- viirs/download-tifs.sh
+++ viirs/download-tifs.sh
@@ -4,16 +4,17 @@
 # you can use monitor-download.sh to monitor the download from outside the tmux
 # download is complete when you have 1054 files
 
-tifs_file='Iowa-download-list.txt'
+TIFS_LIST='Iowa-download-list.txt'
 
 # to get a token, first make an account at https://appeears.earthdatacloud.nasa.gov/
-# then execute: `curl --request POST --user username:password --header "Content-Length: 0" "https://appeears.earthdatacloud.nasa.gov/api/login" > token`
+# then execute: `curl --request POST --user <username>:<password> --header "Content-Length: 0" "https://appeears.earthdatacloud.nasa.gov/api/login" > token`
 # your token will be in the newly generated token file, pass that token as an argument to this script
 # usage: ./download-tifs.sh <token>
 
+# the tifs are placed in a `tifs` subdirectory
+
 TOKEN=$1
 
-while read p; do
-  wget --header "Authorization: Bearer $TOKEN" $p -P ./tifs/
-done < $tifs_file
+mkdir -p tifs
+wget --header "Authorization: Bearer $TOKEN" -i $TIFS_LIST -P ./tifs/
 
